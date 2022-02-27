1c67280fc557ff015894206cf0fce3272cd4030c
Gaurav Nelson <ganelson@outlook.com>
2022-02-27 21:48:18 +1000

    Add viteconfig

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 export default {
     name: 'renderer-asciidoc',
     client: './client.js',
-    server: './server.js'
+    server: './server.js',
+    viteConfig() {
+        return {
+            optimizeDeps: {
+                include: ['renderer-asciidoc/client.js'],
+				exclude: ['renderer-asciidoc/server.js'],
+            },
+            ssr: {
+                renderer: 'renderer-asciidoc/server.js',
+            }
+        };
+    }
+
 }
